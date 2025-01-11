78bad68c55b47518d30e3d367119098592f2ffc7
mehak57 <112238321+mehak57@users.noreply.github.com>
2025-01-11 18:57:47 +0530

    Add files via upload

---
+++ words.js
@@ -0,0 +1,65 @@
+const words =[
+    {
+        word : "group",
+        hint : "A number of object or person"
+    },
+    {
+        word : "taste",
+        hint : "Ability of tongue to detect flavour"
+    },
+    {
+        word : "store",
+        hint : "Large shop where goods are traded"
+    },
+    {
+        word : "field",
+        hint : "Area of land for farming activites"
+    },
+    {
+        word : "friend",
+        hint : " Person other than a family member "
+    },
+    {
+        word : "pocket",
+        hint : "A bag of carrying small itemms"
+    },
+    {
+        word : "pocket",
+        hint : "A bag of carrying small items"
+    },
+    {
+        word: "needle",
+        hint : "A thin and sharp metal pin"
+    },
+    {
+       word : "second",
+       hint : "One-sixtieth of a minute"
+    },
+    {
+        word : "tongue",
+        hint : "The muscular organ of mouth "
+    },
+    {
+       word: "comfort",
+       hint : " A pleasent feeling of relaxation"
+    },
+    {
+        word : "feather",
+        hint : " Hair like outer covering o bird"
+    },
+    {
+        word : "position",
+        hint : "Location of someone or something"
+    },
+    {
+        word: "expansion",
+        hint : "The process of increase or grow"
+    },
+    {
+        word : "country",
+        hint : "A politically identified region"
+    },
+
+
+
+]
